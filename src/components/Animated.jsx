e466b3eda66c6063c74edaa3e3bb6a118f2c2cf1
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-10-22 22:40:21 +1100

    Updated animations so it has a sit transition

--- src/components/Animated.jsx
+++ src/components/Animated.jsx
@@ -1,8 +1,8 @@
 import { motion } from "framer-motion";
 
 const animations = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1 },
+  initial: { opacity: 0, y: -20},
+  animate: { opacity: 1, y: 0},
   exit: { opacity: 0 },
 };
 
