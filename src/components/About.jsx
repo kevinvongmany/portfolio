574a45f88e1778b08318eb40387026a7db8a3fdc
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-10-22 23:43:17 +1100

    Fixed type in About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     return (
       <Animated>
       <h1 className='mb-4 text-blue-500 '>Solution and software development</h1>
-      <p className='text-xl'>As a developer with a decades' worth of technological experience, I often create content-driven integrations and tailored solutions for Content Management platforms</p>
+      <p className='text-xl'>As a developer with over a decade's worth of technological experience, I often create content-driven integrations and tailored solutions for Content Management platforms</p>
       <p className='text-xl mt-3'>
         I have experience building solutions including front end webpages, dashboards, reporting tools, APIs and more
       </p>
