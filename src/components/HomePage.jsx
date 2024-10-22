ce8d13126318d5743eb34072a0796bae883252af
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-10-22 18:35:38 +1100

    Updated homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,8 +3,9 @@ import Animated from './Animated';
 export default function HomePage() {
   return (
     <Animated>
-      <h1>Home Page</h1>
+      <h1 className='mb-4'>Home Page</h1>
       <p>Welcome to the Home Page!</p>
+      <p>UNDER CONSTRUCTION</p>
     </Animated>
   )
 }
