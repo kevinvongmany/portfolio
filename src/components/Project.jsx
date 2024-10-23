c3107d2d84e7cdbc8e45009f78fb3f46764c89c6
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-10-23 21:59:48 +1100

    Added a name tagline for each project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,20 +1,23 @@
 
-export default function Project({ index, image, repository }) {
+export default function Project({ index, image, repository, name }) {
   return (
     <>
-        <a
-          key={index}
-          href={`https://github.com/kevinvongmany/${repository}`}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="block overflow-hidden rounded-lg shadow-lg transform transition-transform hover:scale-105"
-        >
-          <img
-            src={`${image}`}
-            alt={`Project ${index + 1}: ${repository}`}
-            className="w-full h-48 object-cover"
-          />
-        </a>
+      <a
+        key={index}
+        href={`https://github.com/kevinvongmany/${repository}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="block overflow-hidden rounded-lg shadow-lg transform transition-transform hover:scale-105"
+      >
+        <img
+        src={`${image}`}
+        alt={`Project ${index + 1}: ${name}`}
+        className="w-full h-48 object-cover"
+        />
+        <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black to-transparent text-white text-center py-2">
+        <span className="text-lg font-semibold">{name}</span>
+        </div>
+      </a>
     </>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,34 +7,49 @@ import weatherDashboard from '../assets/weather-dashboard.png';
 import pwaTextEditor from '../assets/pwa-text-editor.png';
 import Project from './Project';
 
-const repositories = [
-  'beatbrowser',
-  'cinejournal-webapp',
-  'tech-blog-webpage',
-  'social-network-api',
-  'weather-dashboard',
-  'pwa-text-editor'
-]
-
-const images = [
-  beatbrowser,
-  cinejournal,
-  techBlog,
-  socialNetworkApi,
-  weatherDashboard,
-  pwaTextEditor
+const projects = [
+  {
+    repository: 'beatbrowser',
+    image: beatbrowser,
+    name: 'BeatBrowser'
+  },
+  {
+    repository: 'cinejournal-webapp',
+    image: cinejournal,
+    name: 'CineJournal'
+  },
+  {
+    repository: 'tech-blog-webpage',
+    image: techBlog,
+    name: 'Tech Blog Webpage'
+  },
+  {
+    repository: 'social-network-api',
+    image: socialNetworkApi,
+    name: 'Social Network API'
+  },
+  {
+    repository: 'weather-dashboard',
+    image: weatherDashboard,
+    name: 'Weather Dashboard'
+  },
+  {
+    repository: 'pwa-text-editor',
+    image: pwaTextEditor,
+    name: 'PWA Text Editor'
+  }
 ]
 
 export default function Projects() {
     return (
       <Animated>
-        <h1 className="mb-4 text-blue-500 ">Projects</h1>
-        <h2 className="mb-4 text-2xl">Unfortunately most of my projects are considered proprietary, but feel free to check some of my home projects below!</h2>
+      <h1 className="mb-4 text-blue-500 ">Projects</h1>
+      <h2 className="mb-4 text-2xl">Unfortunately most of my projects are considered proprietary, but feel free to check some of my home projects below!</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {Array.from({ length: 6 }).map((_, index) => (
-          <Project index image={images[index]} repository={repositories[index]} />
+        {projects.map((project, index) => (
+        <Project key={index} image={project.image} repository={project.repository} name={project.name} />
         ))}
-    </div>
+      </div>
       <p className="mt-4 text-gray-400">Please note that some of these projects may be outdated or no longer maintained.</p>
       <h2 className="mt-8 text-2xl">Want to see more? Check out my <a href="https://github.com/kevinvongmany" target="_blank" className="text-blue-600">GitHub</a>!</h2>
       </Animated>
