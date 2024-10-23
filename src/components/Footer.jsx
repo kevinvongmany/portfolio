c776b856d3fe9c75541501a5e4af33befefc5d59
Kevin Vongmany <kevin.vongmany@gmail.com>
2024-10-23 21:01:34 +1100

    fixed footer border

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
 export default function Footer() {
 
     return (
-        <footer className="bg-white dark:bg-gray-900 fixed w-full z-20 bottom-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <footer className="bg-white dark:bg-gray-900 fixed w-full z-20 bottom-0 start-0 border-t border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 text-black dark:text-gray-400">
                 <div className='flex space-x-4'>
                     <a href="https://github.com/kevinvongmany" target="_blank" rel="noopener noreferrer" className="text-black dark:text-gray-400 hover:text-gray-600 dark:hover:text-gray-200">
